b6dbca9290d952a4b1d4aeab29aa0a4367327ff4
Mike Cunneen <mike@appworkshop.net>
2018-02-05 11:50:50 +0800

    tag as appworkshop rather than abernix

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+    #!/bin/bash
 set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "appworkshop/meteord:node-${NODE_VERSION}-base" ../base && \
+  docker tag "appworkshop/meteord:node-${NODE_VERSION}-base" appworkshop/meteord:base
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "appworkshop/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  docker tag "appworkshop/meteord:node-${NODE_VERSION}-onbuild" appworkshop/meteord:onbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "appworkshop/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
+  docker tag "appworkshop/meteord:node-${NODE_VERSION}-devbuild" appworkshop/meteord:devbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "appworkshop/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
+  docker tag "appworkshop/meteord:node-${NODE_VERSION}-binbuild" appworkshop/meteord:binbuild
